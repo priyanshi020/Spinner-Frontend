e2062f16d473b5e7526fd5ba054bb4d4c73ab8b4
priyanshi020 <priyanshirathor19@gmail.com>
2024-11-29 19:58:08 +0530

    validation on spinner

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -7,9 +7,9 @@ import Navbar from "./Navbar";
 import logo from "../Images/tufcon-logo.png";
 import confettiGif from "../Images/confetti.gif"; // Replace with a spinning confetti GIF
 import { useNavigate } from "react-router-dom";
-import betterLuckMp4 from '../Images/oops.mp4'
-import image1 from '../Images/Tiger.png';
-import image2 from '../Images/TMT.png'
+import betterLuckMp4 from "../Images/oops.mp4";
+import image1 from "../Images/Tiger.png";
+import image2 from "../Images/TMT.png";
 const segments = [
   { option: "Better luck" },
   { option: "Won 70 Rs." },
@@ -58,6 +58,15 @@ function Spinner() {
   // };
 
   const handleSpinClick = () => {
+    // Check if the user is allowed to spin
+    const hasSpun = localStorage.getItem("hasSpun");
+  
+    if (hasSpun === "true") {
+      alert("You can't spin again. Please log in to continue.");
+      navigate('/')
+      return;
+    }
+  
     // Reset the spin sound to start from the beginning
     if (spinSound.current) {
       spinSound.current.currentTime = 0; // Reset the audio to the beginning
@@ -69,7 +78,9 @@ function Spinner() {
     // Determine the result based on spin count
     if (spinCount < 2) {
       // Force "Better luck" for the first two spins
-      newPrizeNumber = segments.findIndex((segment) => segment.option === "Better luck");
+      newPrizeNumber = segments.findIndex(
+        (segment) => segment.option === "Better luck"
+      );
     } else {
       // Randomly determine a prize for subsequent spins
       newPrizeNumber = Math.floor(Math.random() * segments.length);
@@ -83,21 +94,23 @@ function Spinner() {
     setShowModal(false);
   };
   
-
   const onFinished = (winner) => {
     setResult(winner);
     setShowModal(true);
     setMustSpin(false);
-
-    // Save prize amount to localStorage if it's not "Better luck"
-    if (!winner.includes("Better luck")) {
+  
+    if (winner.includes("Better luck")) {
+      // Set the "hasSpun" flag in localStorage
+      localStorage.setItem("hasSpun", "true");
+      console.log("Better luck, spin disabled.");
+    } else {
+      // Save prize amount to localStorage if it's not "Better luck"
       const prizeAmount = winner.match(/\d+/)?.[0] || "0"; // Extract the number from the string
       localStorage.setItem("prizeAmount", prizeAmount);
       console.log(`Prize saved in localStorage: ${prizeAmount}`);
-    } else {
-      console.log("No prize won, not saving to localStorage.");
     }
   };
+  
 
   const closeModal = () => {
     setShowModal(false);
@@ -130,97 +143,102 @@ function Spinner() {
           fontFamily="Arial"
         />
 
-        <button className="spin-button" onClick={handleSpinClick} disabled={mustSpin}>
+        <button
+          className="spin-button"
+          onClick={handleSpinClick}
+          disabled={mustSpin}
+        >
           Spin
         </button>
       </div>
 
       <Modal
-  isOpen={showModal}
-  onRequestClose={closeModal}
-  style={{
-    content: {
-      top: "50%",
-      left: "50%",
-      right: "auto",
-      bottom: "auto",
-      transform: "translate(-50%, -50%)",
-      width: "400px",
-      textAlign: "center",
-      padding: "20px",
-      borderRadius: "10px",
-      border: "none",
-      background: "#f8f8f8",
-      boxShadow: "0 4px 10px rgba(0, 0, 0, 0.2)",
-      zIndex: 9999, // Ensure this is above the spinner
-    },
-    overlay: {
-      backgroundColor: "rgba(0, 0, 0, 0.6)",
-      zIndex: 9998, // Slightly lower than content
-    },
-  }}
->
-  {result.includes("Better luck") ? (
-    <video
-      src={betterLuckMp4}
-      autoPlay
-      loop
-      muted
-      width={300}
-      style={{ borderRadius: "10px" }}
-    ></video>
-  ) : (
-    <img
-      src={confettiGif}
-      alt="Confetti"
-      width={300}
-      style={{ borderRadius: "10px" }}
-    />
-  )}
-  <h2 style={{ marginTop: "20px", fontSize: "24px", color: "#333" }}>{result}</h2>
-  {!result.includes("Better luck") && (
-    <button
-      onClick={redirectToForm}
-      style={{
-        marginTop: "20px",
-        background: "#34A24F",
-        color: "#fff",
-        border: "none",
-        padding: "10px 20px",
-        borderRadius: "5px",
-        cursor: "pointer",
-      }}
-    >
-      Claim Your Prize
-    </button>
-  )}
-</Modal>
-
-
-
-    <div>
-      <h2 className="text-center mt-5">Start Spin to see the magic happen!</h2>
-      <div
+        isOpen={showModal}
+        onRequestClose={closeModal}
         style={{
-          display: "flex",
-          flexDirection: "column",
-          alignItems: "center",
-          marginTop: "20px",
+          content: {
+            top: "50%",
+            left: "50%",
+            right: "auto",
+            bottom: "auto",
+            transform: "translate(-50%, -50%)",
+            width: "400px",
+            textAlign: "center",
+            padding: "20px",
+            borderRadius: "10px",
+            border: "none",
+            background: "#f8f8f8",
+            boxShadow: "0 4px 10px rgba(0, 0, 0, 0.2)",
+            zIndex: 9999, // Ensure this is above the spinner
+          },
+          overlay: {
+            backgroundColor: "rgba(0, 0, 0, 0.6)",
+            zIndex: 9998, // Slightly lower than content
+          },
         }}
       >
-        <img
-          src={image1}
-          alt="First Image"
-          style={{ width: "200px", marginBottom: "-1px" }}
-        />
-        <img
-          src={image2}
-          alt="Second Image"
-          style={{ width: "200px", marginTop: "-42px" }}
-        />
+        {result.includes("Better luck") ? (
+          <video
+            src={betterLuckMp4}
+            autoPlay
+            loop
+            muted
+            width={300}
+            style={{ borderRadius: "10px" }}
+          ></video>
+        ) : (
+          <img
+            src={confettiGif}
+            alt="Confetti"
+            width={300}
+            style={{ borderRadius: "10px" }}
+          />
+        )}
+        <h2 style={{ marginTop: "20px", fontSize: "24px", color: "#333" }}>
+          {result}
+        </h2>
+        {!result.includes("Better luck") && (
+          <button
+            onClick={redirectToForm}
+            style={{
+              marginTop: "20px",
+              background: "#34A24F",
+              color: "#fff",
+              border: "none",
+              padding: "10px 20px",
+              borderRadius: "5px",
+              cursor: "pointer",
+            }}
+          >
+            Claim Your Prize
+          </button>
+        )}
+      </Modal>
+
+      <div>
+        <h2 className="text-center mt-5">
+          Start Spin to see the magic happen!
+        </h2>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            marginTop: "20px",
+          }}
+        >
+          <img
+            src={image1}
+            alt="First Image"
+            style={{ width: "200px", marginBottom: "-1px" }}
+          />
+          <img
+            src={image2}
+            alt="Second Image"
+            style={{ width: "200px", marginTop: "-42px" }}
+          />
+        </div>
       </div>
-    </div>
- 
     </div>
   );
 }
