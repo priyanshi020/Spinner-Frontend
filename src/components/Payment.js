36fa90c605d87a63d86404f1a2996907baddbd41
Priyanshi Rathore <74314089+priyanshi020@users.noreply.github.com>
2025-03-07 23:09:49 +0530

    Update Payment.js

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import { API_URL } from "../service";
+import { PAYOUT_URL } from "../service";
 import "./CSS/payment.css";
 import { useNavigate } from "react-router-dom";
 
